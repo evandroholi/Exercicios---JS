a97d1f9d850d47512fa45c520028cc03959ca447
EvandroHOli <55367911+evandroholi@users.noreply.github.com>
2020-01-04 04:50:26 -0300

    Add files via upload

---
+++ Basicos JS/Exec Finais/scriptExe5.js
@@ -0,0 +1,16 @@
+foto = document.getElementById('foto');
+
+function pequena() {
+    foto.width = "100";
+    foto.heigth = "100";
+}
+
+function media() {
+    foto.width = '200';
+    foto.heigth = '200';
+}
+
+function grande() {
+    foto.width = '400';
+    foto.heigth = '400';
+}
---
+++ Basicos JS/Exemplos/meuscript.js
@@ -0,0 +1,10 @@
+original = document.getElementById('olamundo');
+        efeito = original;
+
+        function mudar (){
+            efeito.innerHTML = '<strong>Olá mundo!</strong>';
+            efeito.style = "color: #FF00FF";
+            efeito.style.backgroundColor = "yellow";
+        }
+
+        
